58a5a68c35ec3813f362f48abcaf1cf34c770273
RobertSimpson2619 <RobertSimpson2619@gmail.com>
2017-06-14 21:15:18 -0500

    added html displays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,9 @@ var database = firebase.database();
 
 // Initial Variables
 //*******************************************************************
-
+var minTemp = 0;
+var maxTemp = 0;
+var weatherIcon = "";
 var instagramQueryUrl = "";
 var weatherQueryUrl = "";
 var APIKEY = "a219531e0ef0e795fcea4e7cc6b2e402";
@@ -28,26 +30,14 @@ var weatherApiKey = "&APPID=166a433c57516f51dfab1f7edaed8413";
 
 
 
-
-
-
-
-
-
-
-
-// Ajax call to Weather API 
-//*********************************************************************
-
-
 //Html clicks
 //*********************************************************************
 
 $("#submitButton").on("click",function(){
-	console.log("button clicked");
+	
 	searchLocation = $("#initialInput").val().trim();
 
-	console.log(searchLocation);
+	
 	queryUrl = queryUrl + searchLocation + weatherApiKey;
 
 
@@ -55,13 +45,10 @@ $("#submitButton").on("click",function(){
 	$.get({
   		url: 'https://api.flickr.com/services/feeds/photos_public.gne',
   		dataType: 'jsonp',
-  		data: { "tags": searchLocation, "format": "json" }
+  		data: { "tags": searchLocation, "format": "json"}
 	});
 
 
-
-
-			
     // Get OpenweatherAPI ajax call *************************************
     $.ajax({
       url: queryUrl,
@@ -69,13 +56,65 @@ $("#submitButton").on("click",function(){
     }).done(function(response){
       console.log(response);
 
+    minTemp = ((response.list[0].main.temp_min - 273.15) * 9/5) + 32;
+    maxTemp = ((response.list[0].main.temp_max - 273.15) * 9/5) + 32;
+
+
+    //replace Degrees Farenheit with icon later?
+    minTemp = "Min Temperature: " + Math.floor(minTemp) + " Degrees Farenheit";
+    maxTemp = "Max Temperature: " + Math.floor(maxTemp) + " Degrees Farenheit";	
+
+    weatherIcon = response.list[0].weather[0].icon;
+
+
+      $("#displayedWeather").append("<p> Time: " + response.list[0].dt_txt + "</p>")
+
+      						.append("<p> Decription: " + response.list[0].weather[0].description + "</p>" )
+
+      						.append("<p Humidity >" + response.list[0].main.humidity + " % </p>" )
+
+      						.append("<p>" + minTemp + "</p>" )
+
+      						.append("<p>" + maxTemp + "</p>" )
+
+      						.append("<p> Wind Speed " + response.list[0].wind.speed + "MPH </p>" )
+
+      						.append("<img src = 'http://openweathermap.org/img/w/" + weatherIcon + ".png' style = 'width:100px'>")
+
+      // 		testing				
+      weatherIcon = response.list[0].weather[0].icon;
+      console.log(weatherIcon); 
+
+      
      
+     
+     console.log(response.list[0]);
+
+
     });
 
-
-		
-
+    $("#firstScreenDisplayed").hide();
+    $("#secondScreenDisplayed").show();
 });
+
+
+
+
+
+
+// Prevent Numbers from being entered into the field. 
+
+var number = document.getElementById('initialInput'); 
+
+number.onkeydown = function(e) {
+
+  if (e.keyCode > 47 && e.keyCode < 58){
+  	return false;
+  }
+  else if(e.keyCode > 95 && e.keyCode < 106){
+  	return false;
+  }
+}
 
 
 
@@ -88,9 +127,11 @@ $("#submitButton").on("click",function(){
 function jsonFlickrFeed(json) {
 		  console.log(json);
 
-		  $.each(json.items, function(i, item) {
-		    $("<img />").attr("src", item.media.m).appendTo("#images");
-		  });
+		
+
+		   $.each(json.items, function(i, item) {
+    		$("<img />").attr("src", item.media.m).appendTo("#displayedImages");
+  			});
 		}; 
 
 
